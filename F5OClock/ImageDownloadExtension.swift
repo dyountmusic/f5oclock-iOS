55d5c54a64b91e50e93ed1b48ad574d04bab278e
Daniel Yount <dyount@sentryone.com>
2017-10-13 01:46:30 -0400

    Image is now retrievable through an extension to UIImageView.
    Really need to update the asynchronous logic for data calling.

---
+++ F5OClock/ImageDownloadExtension.swift
@@ -0,0 +1,32 @@
+//
+//  ImageDownloadExtension.swift
+//  F5OClock
+//
+//  Created by Daniel Yount on 10/13/17.
+//  Copyright © 2017 Daniel Yount. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
