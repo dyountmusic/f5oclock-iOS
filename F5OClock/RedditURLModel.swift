84df15c7457cb9bc88751b9a3effb529395d0e01
Daniel <yountdaniel@gmail.com>
2018-03-01 23:19:53 -0500

    Created a RedditURLModel so that I can change the subreddit which it fetches.

---
+++ F5OClock/RedditURLModel.swift
@@ -0,0 +1,25 @@
+//
+//  redditURLmodel.swift
+//  F5OClock
+//
+//  Created by Daniel Yount on 3/1/18.
+//  Copyright © 2018 Daniel Yount. All rights reserved.
+//
+
+import Foundation
+struct RedditURLModel {
+    
+    var redditURL: String {
+        get { return UserDefaults.standard.string(forKey: "RedditURL") ?? "https://www.reddit.com/r/politics/rising.json?sort=new" }
+        set { UserDefaults.standard.set(newValue, forKey: "RedditURL") }
+    }
+    
+    mutating func generateNewRedditURL(withSubredditName: String) {
+        redditURL = "https://www.reddit.com/r/\(withSubredditName)/rising.json?sort=new"
+    }
+    
+    mutating func resetRedditURL() {
+        redditURL = "https://www.reddit.com/r/politics/rising.json?sort=new"
+    }
+    
+}
