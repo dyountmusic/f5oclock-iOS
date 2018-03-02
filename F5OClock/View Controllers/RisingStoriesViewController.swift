d4a3a88098687f9f94e2d62e5a252d581b2ba9bd
Daniel <yountdaniel@gmail.com>
2018-03-02 02:08:43 -0500

    Test

--- F5OClock/View Controllers/RisingStoriesViewController.swift
+++ F5OClock/View Controllers/RisingStoriesViewController.swift
@@ -158,7 +158,6 @@ class RisingStoriesViewController: UIViewController, UITableViewDataSource, UITa
         present(viewControllerToCommit, animated: true)
     }
     
-    
     // MARK: Functions
     
     func updateUI() {
