d59f84180c5b5b2d616f58b02e8a79d444b995eb
Daniel <yountdaniel@gmail.com>
2018-03-01 20:34:41 -0500

    Initial JSON model structure and variables needed declared in new JSON models.

---
+++ F5OClock/JSON Models/RedditChildren.swift
@@ -0,0 +1,15 @@
+//
+//  RedditChildren.swift
+//  F5OClock
+//
+//  Created by Daniel Yount on 3/1/18.
+//  Copyright © 2018 Daniel Yount. All rights reserved.
+//
+
+import Foundation
+
+struct RedditChildren {
+    
+    let data: [RedditPost]
+    
+}
---
+++ F5OClock/JSON Models/RedditPost.swift
@@ -0,0 +1,26 @@
+//
+//  PostRedditAPI.swift
+//  F5OClock
+//
+//  Created by Daniel Yount on 3/1/18.
+//  Copyright © 2018 Daniel Yount. All rights reserved.
+//
+
+import Foundation
+
+struct RedditPost {
+    
+    let title: String
+    let upvotes: Int
+    let url: String
+    let thumbnail: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case upvotes = "ups"
+        case url = "url"
+        case thumbnail = "thumbnail"
+    }
+    
+}
