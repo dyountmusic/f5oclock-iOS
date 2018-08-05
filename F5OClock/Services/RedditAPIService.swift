cb6cfdc4b4ee7d64f52ced334656213f77613dd8
Daniel Yount <yountdaniel@gmail.com>
2018-08-04 21:16:34 -0400

    Filling out the getUserInfo func.

--- F5OClock/Services/RedditAPIService.swift
+++ F5OClock/Services/RedditAPIService.swift
@@ -9,18 +9,26 @@
 import Foundation
 import OAuthSwift
 
-struct RedditRequestModel {
-    var url: String
-    var method: OAuthSwiftHTTPRequest.Method
-    var path: String
-}
-
-
 class RedditAPIService {
     
     var networkServiceModel: NetworkingSerivceModel?
     
     func getUserInfo() {
+        let url = RedditAuthorizationStrings.baseURL.rawValue
+        let path = "/api/v1/me"
+        networkServiceModel?.oauthAuthorizer?.client.get(url + path, success: { (response) in
+            // Success
+            print("Got response!")
+            print(response.dataString())
+            do {
+                let redditUser = try JSONDecoder().decode(RedditUser.self, from: response.data)
+                
+            } catch let jsonError {
+                print("Error serializing JSON from remote server \(jsonError.localizedDescription)")
+            }
+        }, failure: { (error) in
+            // Failure
+        })
         
     }
     
